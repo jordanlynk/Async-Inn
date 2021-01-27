d0cd974e3e5ff978aa11dc2fb95a76c53271820d
Jordan Kidwell <jkid2023@gmail.com>
2021-01-27 15:40:55 -0800

    added in the IRoom and fixed in the corresponding files

--- AsyncInn/AsyncInn/Models/Interfaces/IRoom.cs
+++ AsyncInn/AsyncInn/Models/Interfaces/IRoom.cs
@@ -5,7 +5,12 @@ using System.Threading.Tasks;
 
 namespace AsyncInn.Models.Interfaces
 {
-    interface IRoom
+    public interface IRoom
     {
+        Task<Room> Create(Room room);
+        Task<Room> GetRoom(int Id);
+        Task<List<Room>> GetRooms();
+        Task<Room> UpdateRoom(int Id, Room room);
+        Task DeleteRoom(int Id);
     }
 }
--- AsyncInn/AsyncInn/Startup.cs
+++ AsyncInn/AsyncInn/Startup.cs
@@ -10,6 +10,8 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using AsyncInn.Data;
+using AsyncInn.Models.Interfaces;
+using AsyncInn.Models.Interfaces.Services;
 
 namespace AsyncInn
 {
@@ -24,13 +26,14 @@ namespace AsyncInn
         }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddControllers();
             services.AddDbContext<AsyncInnDbContext>(options => {
                 // Our DATABASE_URL from js days
                 string connectionString = Configuration.GetConnectionString("DefaultConnection");
                 options.UseSqlServer(connectionString);
             });
-            services.AddMvc();
-            services.AddControllers();
+            services.AddTransient<IRoom, RoomRepository>();
 
         }
 
