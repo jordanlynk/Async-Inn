084ce0df6631dd54cb4463d41802bb94ddec4d90
Jordan Kidwell <jkid2023@gmail.com>
2021-01-26 16:31:37 -0800

    Adding in all of the scaffolding for this project.

---
+++ AsyncInn/AsyncInn/Models/Hotels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models
+{
+    public class Hotels
+    {
+        public int ID { get; }
+        public string Name { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
