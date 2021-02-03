bce4b22ef0ca2419a2db340c8f62d5ab2552128f
Jordan Kidwell <jkid2023@gmail.com>
2021-02-02 17:45:52 -0800

    added in mock and tests so far

---
+++ AsyncInn/AsyncTests/Mock.cs
@@ -0,0 +1,34 @@
+﻿using AsyncInn.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AsyncTests
+{
+    public abstract class Mock : IDisposable
+    {
+        private readonly SqliteConnection _connection;
+        protected readonly AsyncInnDbContext _db;
+
+        public Mock()
+        {
+            _connection = new SqliteConnection("Filename=:memory:");
+            _connection.Open();
+            _db = new AsyncInnDbContext(
+                new DbContextOptionsBuilder<AsyncInnDbContext>()
+                .UseSqlite(_connection)
+                .Options);
+            _db.Database.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            _db?.Dispose();
+            _connection?.Dispose();
+        }
+    }
+}
+    
+
---
+++ AsyncInn/AsyncTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace AsyncTests
+{
+    public class UnitTest1 : Mock
+
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
