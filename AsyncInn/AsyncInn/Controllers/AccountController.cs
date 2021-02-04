dddd5c273cb2bfd9996a87f3ce1e8079cff86975
Jordan Kidwell <jkid2023@gmail.com>
2021-02-03 17:55:05 -0800

    successfully got everything working for today's lab and a solid read me.

---
+++ AsyncInn/AsyncInn/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using AsyncInn.Models.DTOs;
+using AsyncInn.Models.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private IUserService userService;
+
+        public AccountController(IUserService service)
+        {
+            userService = service;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<UserDTO>> Register(RegisterUser data)
+        {
+            var user = await userService.Register(data, this.ModelState);
+
+            if (ModelState.IsValid)
+            {
+                return user;
+            }
+
+            return BadRequest(new ValidationProblemDetails(ModelState));
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserDTO>> Login(LoginData data)
+        {
+            var user = await userService.Authenticate(data.Username, data.Password);
+            if (user != null)
+            {
+                return user;
+            }
+            return Unauthorized();
+        }
+
+    }
+}
+
---
+++ AsyncInn/AsyncInn/Models/Interfaces/Services/IdentityUserService.cs
@@ -0,0 +1,75 @@
+﻿using AsyncInn.Models.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.Interfaces.Services
+{
+    public class IdentityUserService : IUserService
+    {
+        private UserManager<ApplicationUser> userManager;
+
+        public IdentityUserService(UserManager<ApplicationUser> manager)
+        {
+            userManager = manager;
+        }
+
+        public async Task<UserDTO> Register(RegisterUser data, ModelStateDictionary modelState)
+        {
+            //throw new NotImplementedException();
+
+            var user = new ApplicationUser
+            {
+                UserName = data.Username,
+                Email = data.Email,
+                PhoneNumber = data.PhoneNumber
+            };
+
+            var result = await userManager.CreateAsync(user, data.Password);
+
+            if (result.Succeeded)
+            {
+                return new UserDTO
+                {
+                    Id = user.Id,
+                    Username = user.UserName
+                };
+            }
+
+            // Put errors into modelState
+            // Ternary:   var foo = conditionIsTrue ? goodthing : bad;
+            foreach (var error in result.Errors)
+            {
+                var errorKey =
+                  error.Code.Contains("Password") ? nameof(data.Password) :
+                  error.Code.Contains("Email") ? nameof(data.Email) :
+                  error.Code.Contains("UserName") ? nameof(data.Username) :
+                  "";
+
+                modelState.AddModelError(errorKey, error.Description);
+            }
+
+            return null;
+        }
+
+        public async Task<UserDTO> Authenticate(string username, string password)
+        {
+            var user = await userManager.FindByNameAsync(username);
+
+            if (await userManager.CheckPasswordAsync(user, password))
+            {
+                return new UserDTO
+                {
+                    Id = user.Id,
+                    Username = user.UserName
+                };
+            }
+
+            return null;
+
+        }
+    }
+}
